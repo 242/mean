6192a6003c65a2572efd4ba2a0f572bfb29437d9
SGio <multiple.entry.ticket@gmail.com>
2014-02-07 02:04:03 +0200

    submit1

---
+++ server.js
@@ -0,0 +1,64 @@
+var express = require('express'),
+	stylus = require('stylus'),
+	mongoose = require('mongoose');
+
+	
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+function compile(str, path){
+	return stylus(str).set('filename', path);
+}
+
+
+app.configure(function(){
+	app.set('views', __dirname + '/server/views');
+	app.set('view engine', 'jade');
+	app.use(express.logger('dev'));
+	app.use(express.json());
+	app.use(express.urlencoded());
+	app.use(stylus.middleware(
+	{
+		src: __dirname + '/public',
+		compile: compile
+	}
+	));
+	app.use(express.static(__dirname + '/public'));
+
+});
+
+app.get('/partials/:partialPath', function(req,res){
+	res.render('partials/'+req.params.partialPath)
+});
+
+app.get('*', function(req,res){
+	res.render('index', {
+		mongoMessage: mongoMessage
+	});
+});
+
+var port = process.env.PORT || 3030;
+app.listen(port);
+console.log("listening on port: "+port );
+
+if (env === 'development'){
+	mongoose.connect('mongodb://localhost/multivision');
+} else {
+	mongoose.connect('mongodb://entrymaker:89jd11m!@ds061558.mongolab.com:61558/muenti');
+}
+var db = mongoose.connection;
+db.on('error', console.error.bind(console,'connection error...'));
+db.once('open', function callback(){
+	console.log('db opened');
+})
+
+var messageSchema = mongoose.Schema({
+	message: String
+});
+
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc){
+	mongoMessage = messageDoc.message;
+})
